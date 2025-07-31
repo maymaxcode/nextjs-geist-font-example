e337ad6d72170623236b89e0c8a14b466c3120d2
maymaxcode <joaofellipe685@gmail.com>
2025-07-31 12:26:59 -0300

    Add .next/static/chunks/src_app_ferramentas_auto-comando_page_tsx_a993527f._.js 1753975619145

---
+++ .next/static/chunks/src_app_ferramentas_auto-comando_page_tsx_a993527f._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_7f1e9a9a._.js"
+],
+    source: "dynamic"
+});
